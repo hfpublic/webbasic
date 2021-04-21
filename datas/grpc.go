d68490451eeeeb20c3015c0f07c45726eea4704d
hfpublic <invincibletortillas@gmail.com>
2021-04-21 14:21:42 +0800

    add grpc

---
+++ datas/grpc.go
@@ -0,0 +1,35 @@
+package datas
+
+import (
+	"fmt"
+	"log"
+
+	"google.golang.org/grpc"
+)
+
+type GRPCConfig struct {
+	Host string
+	Port int
+}
+
+var grpcClient *GRPCClient
+
+type GRPCClient struct {
+	*grpc.ClientConn
+}
+
+func InitGRPCClient(conf GRPCConfig) {
+	conn, err := grpc.Dial(fmt.Sprintf("%s:%v", conf.Host, conf.Port), grpc.WithInsecure())
+	if err != nil {
+		log.Fatalln("dial error:", err)
+	}
+	grpcClient = &GRPCClient{ClientConn: conn}
+}
+
+func GetGRPCClient() *GRPCClient {
+	return grpcClient
+}
+
+func CloseGRPCClient() error {
+	return grpcClient.Close()
+}
